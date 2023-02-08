aa1e3d3a33f691858993bc226607e382acc602aa
Mason Peterson <mbpeterson70@gmail.com>
2023-02-08 15:00:36 -0500

    Fix frontend for use with mobile MOT
    Signed-off-by: Mason Peterson <mbpeterson70@gmail.com>

---
+++ src/config/data_params.py
@@ -0,0 +1,10 @@
+DATA_ROOT_PATH = '/home/masonbp/ford-project/data'
+DYNAMIC_DATA_DIR = DATA_ROOT_PATH + '/dynamic-20230206'
+DYNAMIC_RUNS = [
+    'run0_2023-02-06-18-21-24.bag' # rovers weren't driving
+    'run1_2023-02-06-18-24-57.bag' # hard moving run
+    'run2_2023-02-06-18-32-37.bag' # hard static run
+    'run3_filtered.bag' # easy moving run
+    'run4_2023-02-06-18-52-11.bag' # quick iphone vid
+]
+CAMERA_CALIB_FILE = DYNAMIC_DATA_DIR + '/calib/calib.yaml'
