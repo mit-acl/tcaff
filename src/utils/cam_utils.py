7ff8bc6dd7cda823cc1b35e51670124a9b645fb4
Mason Peterson <mbpeterson70@gmail.com>
2023-03-04 12:54:36 -0500

    use rover artists in dynamic video
    Signed-off-by: Mason Peterson <mbpeterson70@gmail.com>

---
+++ src/utils/cam_utils.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+from utils.transform import transform
+
+def is_viewable(pos, T_WC):
+    cx = 425.5404052734375
+    cy = 400.3540954589844
+    fx = 285.72650146484375
+    fy = 285.77301025390625
+    K = np.array([[fx, 0.0, cx],
+                [0.0, fy, cy],
+                [0.0, 0.0, 1.0]])
+    D = np.array([-0.006605582777410746, 0.04240882024168968, -0.04068116843700409, 0.007674722000956535])
+    width = 800
+    height = 848
+    
+    return pt_is_seeable(K, T_WC, width, height, pos)
+
+def pt_is_seeable(K, T_WC, width, height, pt):
+    #
+    # draw out FOV lines
+    #
+    #              C
+    #             / \
+    #            p1  p0
+    #
+    p0 = np.array([0, height, 1]).reshape((3, 1))
+    p1 = np.array([width, height, 1]).reshape((3, 1))
+    p03d = pixel2groundplane(K, T_WC, p0)
+    p13d = pixel2groundplane(K, T_WC, p1)
+    t = T_WC[:3, 3]
+    
+    l0_slope = (p03d.item(1) - t.item(1)) / (p03d.item(0) - t.item(0))
+    l0_intrcpt = p03d.item(1) - l0_slope*p03d.item(0)
+    l1_slope = (p13d.item(1) - t.item(1)) / (p13d.item(0) - t.item(0))
+    l1_intrcpt = p13d.item(1) - l1_slope*p13d.item(0)
+    
+    # if p03d is to the right of c, cone must be less than
+    if p03d.item(0) > t.item(0):
+        l0_check = lambda x : x.item(1) <= l0_slope * x.item(0) + l0_intrcpt
+    else:
+        l0_check = lambda x : x.item(1) >= l0_slope * x.item(0) + l0_intrcpt
+        
+    # if p13d is to the right of c, cone must be greater than
+    if p13d.item(0) > t.item(0):
+        l1_check = lambda x : x.item(1) >= l1_slope * x.item(0) + l1_intrcpt
+    else:
+        l1_check = lambda x : x.item(1) <= l1_slope * x.item(0) + l1_intrcpt
+        
+    return l0_check(pt) and l1_check(pt)
+
+def pixel2groundplane(K, T_WC, pixel_coord):
+    p_c = np.concatenate([pixel_coord[0:2].reshape(-1), [1]]).reshape((3, 1))
+    p_w = transform(T_WC, np.linalg.inv(K) @ p_c)
+    alpha = -p_w.item(2) / (T_WC[2, 3] - p_w.item(2))
+    return p_w + (T_WC[:3, 3].reshape((3, 1)) - p_w) * alpha
--- tools/dynamic_video.py
+++ tools/dynamic_video.py
@@ -1,28 +1,59 @@
 import json
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, FFMpegWriter
+from matplotlib.transforms import Affine2D
 import numpy as np
 from numpy.linalg import inv
 from scipy.spatial.transform import Rotation as Rot
 import cv2 as cv
 import sys
+import argparse
 
 sys.path.append('../src')
 from utils.transform import transform
+from utils.cam_utils import is_viewable
 
-############# OPTIONS #################3
-RECORD = False
-VIDEO = False
-ROVER_NUM = 2
-ROVER = ['RR01', 'RR04', 'RR06', 'RR08'][ROVER_NUM]
+############# OPTIONS #################
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--output',
+                    type=str,
+                    default='../figs/vid.mp4',
+                    help='file to save video to.')
+parser.add_argument('-f', '--fps',
+                    default=1,
+                    type=int,
+                    help='frames per second.')
+parser.add_argument('-1', '--single-frame', 
+                    default=None,
+                    type=int,
+                    help='only view one frame.')
+parser.add_argument('--spedup',
+                    default=2,
+                    help='speedup.')
+parser.add_argument('-n', '--num-rovers',
+                    default=3,
+                    help='3 or 4')
+parser.add_argument('-r', '--rover',
+                    default='RR06',
+                    type=str,
+                    help='RR01, RR04, RR06, or RR08')
+parser.add_argument('--num-frames',
+                    type=int,
+                    default=None,
+                    help='Number of frames to include in video')
+args = parser.parse_args()
+fps = args.fps
+num_rovers = args.num_rovers
+ROVERS = ['RR04', 'RR06', 'RR08'] if num_rovers == 3 else ['RR01', 'RR04', 'RR06', 'RR08']
+rover = args.rover
 
-if len(sys.argv) > 1:
+if False:
     datafile1 = sys.argv[1]
 else:
-    datafile1 = '/home/masonbp/ford-project/data/mot_metrics/dynamic-final/debug_files/4_rovers/cam_project_test_realign.json'
-    datafile2 = '/home/masonbp/ford-project/data/mot_metrics/dynamic-final/debug_files/4_rovers/cam_project_test_nofix.json'
-video_out = '/home/masonbp/ford-project/data/figure1.mp4'
-video_in = f'/home/masonbp/ford-project/data/dynamic-final/videos/l515/run1_{ROVER}.avi'
+    datafile1 = f'/home/masonbp/ford-project/data/mot_metrics/dynamic-final/debug_files/{num_rovers}_rovers/cam_project_test_realign.json'
+    datafile2 = f'/home/masonbp/ford-project/data/mot_metrics/dynamic-final/debug_files/{num_rovers}_rovers/cam_project_test_nofix.json'
+video_out = args.output
+video_in = f'/home/masonbp/ford-project/data/dynamic-final/videos/l515/run1_{rover}.avi'
 
 fx = 901.47021484375
 fy = 901.8353881835938
@@ -78,6 +109,7 @@ class Viewer():
         for i, datum in enumerate(self.data):
             # new plot setup
             self.axs[0, i].clear()
+            self.axs[1, i].clear()
             self.axs[0, i].set_xlim(self.xlim)
             self.axs[0, i].set_ylim(self.ylim)
             self.axs[0, i].grid(True)
@@ -91,12 +123,24 @@ class Viewer():
             df = datum[df_idx]
             
             for gt in df['groundtruth']:
-                self.axs[0, i].plot(gt[0], gt[1], 'o', color='yellowgreen')
+                num_viewed_by = 0
+                for r in df['rovers']:
+                    T_WC = np.array(df['rovers'][r]['T_WC']).reshape((4,4))
+                    if is_viewable(np.array(gt), T_WC):
+                        num_viewed_by += 1
+                # assert num_viewed_by > 0
+                if num_viewed_by == 0:
+                    color='black'
+                if num_viewed_by == 1:
+                    color='yellowgreen'
+                else:
+                    color='purple'
+                self.axs[0, i].plot(gt[0], gt[1], 'o', color=color)
 
             rover = df['rovers'][self.rover]
+            T_WC = np.array(rover['T_WC']).reshape((4,4))
+            T_WC_bel = np.array(rover['T_WC_bel']).reshape((4,4))
             for track in rover['tracks']:
-                T_WC = np.array(rover['T_WC']).reshape((4,4))
-                T_WC_bel = np.array(rover['T_WC_bel']).reshape((4,4))
 
                 track_corrected = transform(T_WC @ inv(T_WC_bel), np.array(track + [0]))
                 track_corrected[2] = 0
@@ -111,49 +155,61 @@ class Viewer():
                     pixel_coords = uvs.reshape(-1)[:2]
                     pixel_coords /= 1
                     cv.drawMarker(frame, tuple(pixel_coords[:].astype(int).tolist()), self.get_track_color(0), cv.MARKER_TILTED_CROSS, thickness=10, markerSize=90)
-                
-            # x, y = self.pts_from(T_WC)
-            # # import ipdb; ipdb.set_trace()
-            # self.axs[0, i].plot(x[0], y[0], 'o', color='red')
-            # self.axs[0, i].plot(x, y, color='red')
 
             for r in df['rovers']:
                 T_WR = np.array(df['rovers'][r]['T_WC']).reshape((4,4))
-                x, y = self.pts_from(T_WR)
+                T_WR = self.T2pltT(T_WR, self.axs[0, i])
+                # x, y = self.pts_from(T_WR)
                 # import ipdb; ipdb.set_trace()
-                ms = 1
-                self.axs[0, i].plot(x[0], y[0], 'o', color='red', markerSize=ms)
-                self.axs[0, i].plot(x, y, color='red')
+                # ms = 4
+                self.draw_rover(self.axs[0, i], T_WR, ghost=True)
+                # self.axs[0, i].plot(x[0], y[0], 'o', color='red', markerSize=ms)
+                # self.axs[0, i].plot(x, y, color='red')
 
-            rs = ['RR01', 'RR04', 'RR06', 'RR08']
             for r, T_fix in rover['T_fix'].items():
                 T_fix = np.array([T_fix]).reshape((4,4))
-                r_name = rs[int(r) % 4]
+                r_name = ROVERS[int(r) % len(ROVERS)]
                 T_j_hat = np.array([df['rovers'][r_name]['T_WC_bel']]).reshape((4,4))
                 T_j = T_WC @ inv(T_WC_bel) @ T_fix @ T_j_hat
-                x, y = self.pts_from(T_j)
+                T_j = self.T2pltT(T_j, self.axs[0, i])
+                # x, y = self.pts_from(T_j)
                 # import ipdb; ipdb.set_trace()
-                self.axs[0, i].plot(x[0], y[0], 'o', color='orange', markerSize=ms)
-                self.axs[0, i].plot(x, y, color='orange')
+                # self.axs[0, i].plot(x[0], y[0], 'o', color='orange', markerSize=ms)
+                # self.axs[0, i].plot(x, y, color='orange')
+                self.draw_rover(self.axs[0, i], T_j)
+                self.axs[0, i].set_aspect(1)
 
             
             self.axs[1, i].imshow(frame[...,::-1])
 
         return
+    
+    def T2pltT(self, T, ax):
+        T[:3, :3] = T[:3,:3] @ self.cam2notcam
+        T = np.delete(T, [2], axis=0)
+        T = np.delete(T, [2], axis=1)
+        T = Affine2D(T) + ax.transData
+        return T
 
     def pts_from(self, T_WC):
         x = [T_WC[0,3]] 
         y = [T_WC[1,3]]
-        length = .1
-        R = Rot.from_matrix(self.cam2notcam @ T_WC[:3,:3])
+        length = .5
+        R = Rot.from_matrix(T_WC[:3,:3] @ self.cam2notcam )
         th = R.as_euler('xyz', degrees=False)[2]
         x.append(T_WC[0,3] + length * np.cos(th))
         y.append(T_WC[1,3] + length * np.sin(th))
         return x, y
             
     
-    def draw_rover(self, ax, T_WB):
-        s = 4 # scale factor
+    def draw_rover(self, ax, T_WB, ghost=False):
+        if ghost:
+            rover_color = '#dbb4bc'
+            wheel_color = 'gray'
+        else:
+            rover_color = '#DC143C'
+            wheel_color = 'k'
+        s = 2 # scale factor
         WIDTH = 0.381 * s
         LENGTH = 0.508 * s
         WHEELR = 0.222 * s # radius of wheel
@@ -161,20 +217,21 @@ class Viewer():
         WHEELT = (0.497 * s - WIDTH) / 2. # wheel thickness
 
         bl = - np.r_[LENGTH, WIDTH] / 2.
-        body = plt.Rectangle(bl, LENGTH, WIDTH, color='#DC143C', transform=T_WB)
+        body = plt.Rectangle(bl, LENGTH, WIDTH, color=rover_color, transform=T_WB)
+        # print(bl)
 
         wheels = []
         wbl = - np.r_[LENGTH, WHEELD] / 2. - np.r_[0, WHEELT] * 1.7 + np.r_[0.01 * s, 0]
-        wheels.append(plt.Rectangle(wbl, WHEELR, WHEELT, color='k', transform=T_WB))
+        wheels.append(plt.Rectangle(wbl, WHEELR, WHEELT, color=wheel_color, transform=T_WB))
         wbl = wbl + np.r_[WHEELD, 0]
-        wheels.append(plt.Rectangle(wbl, WHEELR, WHEELT, color='k', transform=T_WB))
+        wheels.append(plt.Rectangle(wbl, WHEELR, WHEELT, color=wheel_color, transform=T_WB))
         wbl = wbl + np.r_[0, WIDTH] + np.r_[0, WHEELT] / 2
-        wheels.append(plt.Rectangle(wbl, WHEELR, WHEELT, color='k', transform=T_WB))
+        wheels.append(plt.Rectangle(wbl, WHEELR, WHEELT, color=wheel_color, transform=T_WB))
         wbl = wbl - np.r_[WHEELD, 0]
-        wheels.append(plt.Rectangle(wbl, WHEELR, WHEELT, color='k', transform=T_WB))
+        wheels.append(plt.Rectangle(wbl, WHEELR, WHEELT, color=wheel_color, transform=T_WB))
 
         pts = np.array([[0, - 0.8 * WIDTH / 2], [ 1.3 * WIDTH / 2, 0], [0, 0.8 * WIDTH / 2]])
-        orientation = plt.Polygon(pts, closed=True, color='k', transform=T_WB)
+        orientation = plt.Polygon(pts, closed=True, color=wheel_color, transform=T_WB)
 
         # camera FOV frustrum
         near = 0.1 * s
@@ -189,24 +246,27 @@ class Viewer():
         pts += np.c_[LENGTH/2.,0]
         frustum = plt.Polygon(pts, closed=True, color='b', alpha=0.1, transform=T_WB)
 
-        label = plt.text(0.6 * -LENGTH/2, 0.2 * WIDTH/2, 0, fontsize='small', transform=T_WB,
-                                transform_rotates_text=True, rotation=-90, rotation_mode='anchor',
-                                horizontalalignment='center', verticalalignment='center',
-                                color='white')
+        # label = plt.text(0.6 * -LENGTH/2, 0.2 * WIDTH/2, 0, fontsize='small', transform=T_WB,
+        #                         # transform_rotates_text=True, 
+        #                         rotation=-90, rotation_mode='anchor',
+        #                         horizontalalignment='center', verticalalignment='center',
+        #                         color='white')
 
         for wheel in wheels:
             ax.add_patch(wheel)
         ax.add_patch(body)
         ax.add_patch(orientation)
         ax.add_patch(frustum)
+        # print(ax.add_patch)
+        # ax.add_patch(plt.Rectangle((0.5, 0.5), 2, 2, color=wheel_color))
 
-        artist = {'body': body, 'orientation': orientation, 'wheels': wheels, 'frustum': frustum, 'label': label}
+        artist = {'body': body, 'orientation': orientation, 'wheels': wheels, 'frustum': frustum}
 
         artist['body'].set_transform(T_WB)
         [w.set_transform(T_WB) for w in artist['wheels']]
         artist['orientation'].set_transform(T_WB)
         artist['frustum'].set_transform(T_WB)
-        artist['label'].set_transform(T_WB)
+        # artist['label'].set_transform(T_WB)
 
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
@@ -222,23 +282,22 @@ class Viewer():
         return tuple(v.item() for v in c[::-1])
 
 
-viewer = Viewer([datafile1, datafile2], video_in, ROVER)
-# viewer.single_frame_view(150)
+viewer = Viewer([datafile1, datafile2], video_in, rover)
 start_frame = 125*30 #3750 #120*30
-fps = 15
-num_frames = (180-125) * fps
-start_frame=4065
-# viewer.view(4200)
-viewer.view(start_frame)
-# viewer.fig.subplots_adjust(wspace=.3, hspace=.3)
-viewer.fig.set_dpi(300)
-# ani = FuncAnimation(viewer.fig, lambda i: viewer.view(i*30/fps + start_frame), frames=num_frames, interval=1, repeat=False)    
+viewer.fig.subplots_adjust(wspace=.3, hspace=.3)
+if not args.num_frames:
+    num_frames = (180-125) * fps
+else:
+    num_frames = args.num_frames
+# viewer.fig.set_dpi(300)
 
-if RECORD:
+if not args.single_frame:
     # saving to m4 using ffmpeg writer
+    ani = FuncAnimation(viewer.fig, lambda i: viewer.view(i*30/fps + start_frame), frames=num_frames, interval=1, repeat=False) 
     print('saving video...')
     writervideo = FFMpegWriter(fps=int(fps*2))
     ani.save(video_out, writer=writervideo)
     plt.close()
 else:
+    viewer.view(args.single_frame)
     plt.show()
