d2c8a0dedd2b3ccb279819143ba3af9fca84f221
Mason Peterson <mbpeterson70@gmail.com>
2023-04-22 11:47:37 -0400

    Add more options for dynamic test plotting
    Signed-off-by: Mason Peterson <mbpeterson70@gmail.com>

--- tools/dynamic_plotter.py
+++ tools/dynamic_plotter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import json
 
 def smooth_scores(scores):
     scores = np.array(scores)
@@ -21,35 +22,56 @@ def smooth_scores(scores):
     scores = scores_smoothed
     return scores
 
-def get_float_val(line, identifier):
+def get_float_val(line, identifier, is_list=False):
     try:
-        return float(line.split(identifier)[1].strip().split()[0])
+        if not is_list:
+            return float(line.split(identifier)[1].strip().split()[0])
+        else:
+            json.loads(line.split(identifier)[1].strip())
+            # import ipdb; ipdb.set_trace()
+            return json.loads(line.split(identifier)[1].strip())
     except:
         print('offending line:')
         print(line)
         print(identifier)
         assert False
 
-def read_scores(metric_file, metric):
+def read_scores(metric_file, args):
+    metric = args.metric
     scores = []
     T_diffs = []
     with open(metric_file, 'r') as f:
+        new_score = [None, None, None, None]
         for line in f.readlines():
             if metric in line:
-                new_score = [[], [], [], []]
-                new_score[0] = get_float_val(line, f'{metric}:')
+                assert new_score[0] is None
+                new_score[0] = get_float_val(line, f'{metric}:', is_list=args.metric_is_list)
+                if None not in new_score: 
+                    scores.append(new_score)
+                    new_score = [None, None, None, None]
             elif 'T_mag' in line:
+                assert new_score[1] is None
                 new_score[1] = get_float_val(line, 'T_mag:')
+                if None not in new_score: 
+                    scores.append(new_score)
+                    new_score = [None, None, None, None]
             elif 'psi_diff:' in line:
+                assert new_score[2] is None
                 new_score[2] = get_float_val(line, 'psi_diff:')
+                if None not in new_score: 
+                    scores.append(new_score)
+                    new_score = [None, None, None, None]
             elif 't_diff:' in line:
+                assert new_score[3] is None
                 new_score[3] = get_float_val(line, 't_diff:')
-                scores.append(new_score)
-                new_score = None
-            
-            if 'T_diff_list:' in line:
-                pass
+                if None not in new_score: 
+                    scores.append(new_score)
+                    new_score = [None, None, None, None]
     return scores
+
+############################################
+########### Args Setup #####################
+############################################
 
 parser = argparse.ArgumentParser(description='Annotate video sequence with ground truth')
 parser
@@ -66,6 +88,10 @@ parser.add_argument('--window-len',
                     type=float)
 parser.add_argument('--num-lines', '-n', default=None, type=int)
 parser.add_argument('--metric', '-m', default='mota', type=str)
+parser.add_argument('--metric-only', action='store_true')
+parser.add_argument('--metric-is-list', action='store_true')
+parser.add_argument('--metric-plot-avg', action='store_true')
+parser.add_argument('--output', '-o', default=None, type=str)
 args = parser.parse_args()
 
 if not args.metric_file:
@@ -73,6 +99,11 @@ if not args.metric_file:
                 '/home/masonbp/ford-project/data/mot_metrics/dynamic-final/3_rovers/no_fix.yaml',
                 '/home/masonbp/ford-project/data/mot_metrics/dynamic-final/3_rovers/wls_22_1s.yaml',
                 '/home/masonbp/ford-project/data/mot_metrics/dynamic-final/3_rovers/smart_R_2_1_Rfagain.yaml']
+metric = args.metric
+
+############################################
+############## Parsing  ####################
+############################################
 
 score_list = []
 if args.num_lines is None:
@@ -80,72 +111,75 @@ if args.num_lines is None:
 else:
     num_lines = args.num_lines
 for metric_file in args.metric_file[:num_lines]:
-    scores = read_scores(metric_file, args.metric)
+    scores = read_scores(metric_file, args)
     if 'old_mes' in metric_file:
         score_list.append(smooth_scores(scores))
-    else:
+    elif not args.metric_is_list:
         score_list.append(np.array(scores))
-            
+    else:
+        score_list.append(scores)
 
-
-
-
-f, (ax) = plt.subplots(3, 1, sharex=True, figsize=[8, 4.8])
+############################################
+############## Plotting  ###################
+############################################
 
 colors = ['tab:red', 'navy', 'tab:orange', 'tab:green']
-# top plot MOTA
-ts = list()
-for i, s in enumerate(score_list):
-    print(s.shape)
-    t = [*range(s.shape[0])]
-    t = np.array(t) * args.sample_len
-    # print(t.shape)
-    # print(s[:, 0].shape)
-    ax[0].plot(t, s[:, 0], color=colors[i])
-# ax1.plot(t, score_list[1][:, 0] - score_list[0][:,  0])
-ax[0].set_ylabel('MOTA')
-ax[0].set_ylim([0, 1])
-# ax[0].legend(['VICON odometry', 'WLS realignment', 'original realignment'])
-# ax[0].legend(['VICON odometry', 'WLS realignment', 'L515 detections'])
-# ax[0].legend(['t265 w/ realignment', 't265 no realignment'])
 
-minor_ticks = np.arange(0, t[-1], 5)
-# ax[0].set_xticks(minor_ticks, minor=True)
-# ax[0].grid(which='both')
+if not args.metric_only:
+    f, (ax) = plt.subplots(3, 1, sharex=True, figsize=[8, 4.8])
 
-# mid plot T_diff
-for i, s in enumerate(score_list):
-    print(s.shape)
-    t = [*range(s.shape[0])]
-    t = np.array(t) * args.sample_len
-    ax[1].plot(t, s[:, 2], color=colors[i])
-    ax[2].plot(t, s[:, 3], color=colors[i])
-ax[1].set_ylabel('Heading error (deg)')
-ax[1].grid(True)
-# ax[1].set_xticks(minor_ticks, minor=True)
-# ax[1].grid(which='both')
-ax[2].set_ylabel('Translation error (m)')
-ax[2].set_xlabel('Time (s)')
-ax[1].legend(['Perfect localization', 'Casao', 'MOTLEE (realignment)', 'MOTLEE (realignment +\n uncertainty incorporation)'], loc=(0.01,0.3))
-# ax[2].set_xticks(minor_ticks, minor=True)
-# ax[2].grid(which='both')
+    # top plot MOTA
+    ts = list()
+    for i, s in enumerate(score_list):
+        t = [*range(s.shape[0])]
+        t = np.array(t) * args.sample_len
+        ax[0].plot(t, s[:, 0], color=colors[i])
+    ax[0].set_ylabel('MOTA')
+    ax[0].set_ylim([0, 1])
 
-ax[1].set_ylim([-.9, 17])
-ax[2].set_ylim([-.15, 3.1])
+    minor_ticks = np.arange(0, t[-1], 5)
+
+    # mid plot T_diff
+    for i, s in enumerate(score_list):
+        t = [*range(s.shape[0])]
+        t = np.array(t) * args.sample_len
+        ax[1].plot(t, s[:, 2], color=colors[i])
+        ax[2].plot(t, s[:, 3], color=colors[i])
+    ax[1].set_ylabel('Heading error (deg)')
+    ax[1].grid(True)
+    ax[2].set_ylabel('Translation error (m)')
+    ax[2].set_xlabel('Time (s)')
+    ax[1].legend(['Perfect localization', 'Casao', 'MOTLEE (realignment)', 'MOTLEE (realignment +\n uncertainty incorporation)'], loc=(0.01,0.3))
+
+    ax[1].set_ylim([-.9, 17])
+    ax[2].set_ylim([-.15, 3.1])
+
+    for axi in ax:
+        axi.grid(True)
+
+else:
+    f, ax = plt.subplots()
+    ax.grid(True)
+    ts = list()
+    for i, s in enumerate(score_list):
+        t = [*range(len(s))]
+        t = np.array(t) * args.sample_len
+        if not args.metric_is_list:
+            ax.plot(t, s[:, 0], color=colors[i])
+        elif not args.metric_plot_avg:
+            for j, step in enumerate(s):
+                data_pts = step[0]
+                ax.scatter(np.ones(len(data_pts))*t[j], data_pts, color=colors[i])
+    ax.set_ylabel(metric)
+    if metric == 'mota':
+        ax.set_ylim([0, 1])
 
 
-for axi in ax:
-    axi.grid(True)
 
-# # bottom plot T_mag
-# ax[-1].plot(t, score_list[-1][:, 1])
-# ax[-1].set_xlabel('t (s)')
-# ax[-1].set_ylabel('T_mag (m)')
-# ax[-1].grid(True)
+# f.set_dpi(240)
 
-f.set_dpi(240)
-# plt.show()
-
-# with open('/home/masonbp/fig.png', 'w') as fh:
-# plt.savefig('/home/masonbp/fig.png', format='png')
-plt.show()
+if args.output is not None:
+    with open(args.output, 'w') as fh:
+        plt.savefig(args.output, format='png')
+else:
+    plt.show()
