fb09c97977edf1be0118ad9bb8a7fd96dce6cad5
Mason Peterson <mbpeterson70@gmail.com>
2024-01-10 16:47:24 -0500

    add 3d person detection using depth camera
    Signed-off-by: Mason Peterson <mbpeterson70@gmail.com>

---
+++ demo/person_detector_3d.py
@@ -0,0 +1,55 @@
+import numpy as np
+from yolov7_package import Yolov7Detector
+from motlee.utils.cam_utils import depth_mask_2_centroid
+
+class PersonDetector3D():
+    
+    def __init__(self, K, depth_scale=.001, img_size=(256, 256)):
+        self.K = K
+        self.depth_scale=depth_scale
+        self.detector = Yolov7Detector(traced=False, img_size=img_size)
+        
+    def masks(self, img, scale1d=.5):
+        """
+        Creates images masks for detected people
+
+        Args:
+            img (cv image or numpy array): Query image
+            scale1d (float, optional): Return masks that are scaled along each axis by this value. 
+                This is useful for creating centered masks for depth detection. Defaults to .5.
+
+        Returns:
+            list: List of numpy arrays containing person masks
+            list: List of pixel centroids
+        """
+        classes, boxes, scores = self.detector.detect(img)
+        person_boxes = []
+        for i, cl in enumerate(classes[0]):
+            if self.detector.names[cl] == 'person':
+                person_boxes.append(boxes[0][i])
+
+        masks = [np.zeros(img.shape[:2]).astype(np.int8) for _ in person_boxes]
+        centroids = []
+        for box, mask in zip(person_boxes, masks):
+            x0, y0, x1, y1 = np.array(box).astype(np.int64).reshape(-1).tolist()
+            w, h = x1 - x0, y1 - y0
+            xc, yc = x0 + w / 2, y0 + h / 2
+            x0, x1 = int(xc - w*.5*scale1d), int(xc + w*.5*scale1d)
+            y0, y1 = int(yc - h*.5*scale1d), int(yc + h*.5*scale1d)
+            # x0 = max(x0, 0)
+            # y0 = max(y0, 0)
+            # x1 = min(x1, mask.shape[1])
+            # y1 = min(y1, mask.shape[0])
+            mask[y0:y1,x0:x1] = np.ones((y1-y0, x1-x0)).astype(np.int8)
+            centroids.append((xc, yc))
+        return masks, centroids
+        
+    def detect(self, img_color, img_depth):
+        assert img_color.shape[:2] == img_depth.shape[:2], "Depth and color image shapes do not match"
+        masks, centroids_pixels = self.masks(img_color)
+        centroids_3d = []
+        for mask, cent_pix in zip(masks, centroids_pixels):
+            centroids_3d.append(depth_mask_2_centroid(img_depth, mask, cent_pix, self.K, self.depth_scale))
+            
+        return centroids_3d
+        
--- motlee/utils/cam_utils.py
+++ motlee/utils/cam_utils.py
@@ -54,3 +54,72 @@ def pixel2groundplane(K, T_WC, pixel_coord):
     p_w = transform(T_WC, np.linalg.inv(K) @ p_c)
     alpha = -p_w.item(2) / (T_WC[2, 3] - p_w.item(2))
     return p_w + (T_WC[:3, 3].reshape((3, 1)) - p_w) * alpha
+
+def get_3d_coordinates(K, d, x, y):
+    """Written by ChatGPT and fixed by me
+
+    Args:
+        K ((3,3) np.array): camera intrinsic matrix
+        d (float): depth (distance along z-axis)
+        x (float): pixel x coordinate
+        y (float): pixel y coordinate
+
+    Returns:
+        (3,) np.array: x, y, z 3D point coordinates in camera RDF coordinates
+    """
+    # Step 1: Convert pixel coordinates to normalized device coordinates (NDC)
+    cx = K[0, 2]
+    cy = K[1, 2]
+    fx = K[0, 0]
+    fy = K[1, 1]
+    
+    NDC_x = (x - cx) / fx
+    NDC_y = (y - cy) / fy
+    
+    # Step 2: Get the 3D world coordinates (W)
+    W_x = NDC_x * d
+    W_y = NDC_y * d
+    W_z = d
+    return np.array([W_x, W_y, W_z])
+
+
+def depth_mask_2_centroid(depth, mask, center_pixel, K, depth_scale=1e-3):
+    """Processes a depth image and binary mask to output a 3D centroid measurement of the segmented object
+
+    Args:
+        depth ((h,w) uint16 np.array):      depth image array
+        mask ((h,w) binary np.array):   segment mask
+        center_pixel (2-element int tuple): x, y from top left corner
+        K ((3,3) np.array):                 camera intrinsic matrix
+        depth_scale (float):                amount to multiply depth image by
+
+    Returns:
+        (3,) np.array: centroid 3d position
+    """    
+    depth_masked = depth.copy().astype(np.float64)
+    depth_masked[mask==0] = np.nan
+    depth_masked[depth_masked < 1.] = np.nan
+
+    depth_med = np.nanmedian(depth_masked.reshape(-1))
+    
+    centroid = get_3d_coordinates(K, depth_med*depth_scale, center_pixel[0], center_pixel[1])
+    
+    return centroid
+
+def mask_depth_2_width_height(depth, mask, K):
+    # find bbox corners
+    nonzero_indices = np.transpose(np.nonzero(mask))
+    x0 = np.min(nonzero_indices[:,1])
+    x1 = np.max(nonzero_indices[:,1])
+    y0 = np.min(nonzero_indices[:,0])
+    y1 = np.max(nonzero_indices[:,0])
+    
+    # get 3D points of 2D bounding box using median depth
+    pt0 = get_3d_coordinates(K, depth, x0, y0)
+    pt1 = get_3d_coordinates(K, depth, x1, y1)
+
+    # calculate and return width and height
+    width = pt1.item(0) - pt0.item(0)
+    height = pt1.item(1) - pt0.item(1)
+    return width, height
+    
